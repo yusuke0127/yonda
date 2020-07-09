9ba2ea3141c200655ff623da39e79d12816b2652
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-10 00:00:51 +0900

    Added show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @posts = policy_scope(Post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    authorize @post
   end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,8 @@ class PostPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
 end
