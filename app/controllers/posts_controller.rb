efe5c6075348965cd04d51c3b1f1cd45d4dc1177
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-10 00:34:20 +0900

    Added create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,33 @@ class PostsController < ApplicationController
 
   def index
     @posts = policy_scope(Post)
+    @post = Post.new
   end
 
   def show
     @post = Post.find(params[:id])
     authorize @post
+  end
+
+  def new
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    authorize @post
+    if @post.save!
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -8,4 +8,8 @@ class PostPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def create?
+    true
+  end
 end
