ad5e9e7f95b0fdcc60562117b3f4c7e01217a9f8
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-10 00:37:44 +0900

    Fix order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @posts = policy_scope(Post)
+    @posts = policy_scope(Post).order(created_at: :desc)
     @post = Post.new
   end
 
