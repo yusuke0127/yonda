57a006451f6cc0c7beef0e9d73507624ac980619
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-09 16:51:48 +0900

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts
 
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
