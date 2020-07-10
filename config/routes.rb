77459ca88714928e9a1ce6eea6e7d8bbe059a00a
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-10 15:52:53 +0900

    Added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'posts#index'
   resources :posts do
     resources :comments, only: [:index, :new, :create, :edit, :update]
   end
