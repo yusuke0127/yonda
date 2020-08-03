35db07b8359752c4bc6a0a78ac7b9510a94be48c
yusuke ishida <ishidayusuke01@gmail.com>
2020-08-03 17:17:46 +0900

    Added system test

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,9 +1,28 @@
 require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit posts_url
 
+  test "visiting the index" do
+    visit "/"
     assert_selector "h3", text: "Make a Thread"
+    assert_selector ".post-info-title", count: Post.count
+  end
+
+
+  test "lets a signed in user create a new post" do
+    login_as users(:yusuke)
+    visit "/posts/new"
+    # save_and_open_screenshot
+
+    fill_in "post_title", with: "Le Wagon"
+    fill_in "post_content", with: "Change your life: Learn to code"
+    # save_and_open_screenshot
+
+    click_on 'Confirm'
+    # save_and_open_screenshot
+
+    # Should be redirected to Home with new product
+    # assert_equal post_path(:id), page.current_path
+    assert_text "Change your life: Learn to code"
   end
 end
