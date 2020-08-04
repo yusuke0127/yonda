a94a934b9f917499a1ce03e87a083dc0a6fdbfe8
yusuke ishida <ishidayusuke01@gmail.com>
2020-08-04 19:03:22 +0900

    Added feature tests

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -18,4 +18,44 @@ RSpec.feature "Posts", type: :feature do
       expect(page).to have_content "#{user.email}"
     end.to change(user.posts, :count).by(1)
   end
+
+  scenario "user adds a new comment" do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post, user: user)
+
+    visit root_path
+    click_link "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    visit(post_path(post))
+
+    expect do
+      fill_in "What's your thoughts?", with: "Test Comment"
+      click_button "Comment"
+      expect(page).to have_content "Test Comment"
+    end.to change(post.comments, :count).by(1)
+  end
+
+  # scenario "user likes a post" do
+  #   user = FactoryBot.create(:user)
+  #   post = FactoryBot.create(:post, user: user)
+
+  #   visit root_path
+  #   click_link "Login"
+  #   fill_in "Email", with: user.email
+  #   fill_in "Password", with: user.password
+  #   click_button "Log in"
+
+  #   visit(post_path(post))
+
+  #   save_and_open_page
+  #   expect do
+  #     within "post-info-comment-count" do
+  #       click_link "/posts/#{post.id}/vote.upvote"
+  #     end
+  #   end.to change(post.get_upvotes, :size).by(1)
+
+  # end
 end
