c5d7fe8319dc0f2bd391844b24f3db8b618bd3a7
yusuke ishida <ishidayusuke01@gmail.com>
2020-08-05 16:26:37 +0900

    Refactor some codes

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -18,6 +18,15 @@ RSpec.feature "Posts", type: :feature do
     end.to change(user.posts, :count).by(1)
   end
 
+  scenario "user edits a post" do
+    login_as user, scope: :user
+
+    visit(post_path(post))
+    click_link('edit')
+
+    expect(page).to have_content "Edit your post"
+  end
+
   scenario "user adds a new comment" do
     # user = FactoryBot.create(:user)
     # post = FactoryBot.create(:post, user: user)
@@ -57,24 +66,22 @@ RSpec.feature "Posts", type: :feature do
 
     expect(page).to have_content "Log in"
   end
-  # scenario "user likes a post" do
-  #   user = FactoryBot.create(:user)
-  #   post = FactoryBot.create(:post, user: user)
 
-  #   visit root_path
-  #   click_link "Login"
-  #   fill_in "Email", with: user.email
-  #   fill_in "Password", with: user.password
-  #   click_button "Log in"
+  scenario "user likes a post" do
+    login_as user, scope: :user
+    visit(post_path(post))
 
-  #   visit(post_path(post))
+    expect do
+      click_link "upvote"
+    end.to change(post.get_upvotes, :size).by(1)
+  end
 
-  #   save_and_open_page
-  #   expect do
-  #     within "post-info-comment-count" do
-  #       click_link "/posts/#{post.id}/vote.upvote"
-  #     end
-  #   end.to change(post.get_upvotes, :size).by(1)
+  scenario "user dislikes a post" do
+    login_as user, scope: :user
+    visit(post_path(post))
 
-  # end
+    expect do
+      click_link "downvote"
+    end.to change(post.get_downvotes, :size).by(1)
+  end
 end
